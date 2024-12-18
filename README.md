90018da9eeaf1e29fca5621fe8713d7d717b8e3b
lukasz-kam <lukasz.kaminski02@gmail.com>
2024-12-18 14:13:35 +0100

    Add configuration for creating bastion and secured instances on EC2

---
+++ README.md
@@ -0,0 +1,53 @@
+# bastion-host
+
+The code in the repo is used to create 2 AWS EC2 instances.
+Bastion instance is used to access secured instance.
+Direct connection to secured instance is prohibited through security group.
+The connection between the two is made inside the VPC.
+Bastion sshd is configured to allow connections to secured instance only by port 22.
+Both instances are configured to allow connections with ssh keys
+signed by trusted CA which is created by ansible playbook.
+Ansible playbook is also used to generate temporary ssh key
+and add ssh hosts configuration to ~/.ssh/config for easily
+accessing bastion and secured instances.
+
+### 1. Create EC2 instances with terraform
+  ```
+  cd terraform
+  terraform init
+  terraform apply
+  ```
+
+### 2. Copy ssh configs, create CA files, create temporary ssh key
+  ```
+  cd ansible
+  ansible-playbook main.yml -i inventory.ini
+  ansible-playbook generate_key.yml -i inventory.ini
+  ```
+
+### 3. Change security groups port and cidr_block with terraform
+  ```
+  # Changes to be made are commented im main.tf file
+  cd terraform
+  terraform apply
+  ```
+
+### 4. Accessing instances
+  ```
+  ssh bastion -p 37271
+  ssh secured -J bastion:37271
+  ```
+
+### Addidional info
+
+  Ansible playbook main.tf changes bastion ssh port to 37271 so it is needed
+  to change 22 port in bastion security group to 37271.
+  Secured instance cidr_block in security group is also needed to be changed
+  to bastion private ip for restricting access from another sources.
+
+  In case of ssh connection error ```Too many authentication failures```
+  you need to clear the memory of ssh agent with command:
+
+   ```ssh-add -D```
+
+### Link to the project details: [https://roadmap.sh/projects/bastion-host](https://roadmap.sh/projects/bastion-host).
---
+++ terraform/main.tf
@@ -0,0 +1,93 @@
+resource "aws_security_group" "bas_sg" {
+  name_prefix = "bas-sg-"
+
+  ingress {
+    from_port   = 22 #37271
+    to_port     = 22 #37271
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "sec_sg" {
+  name_prefix = "sec-sg-"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #["${aws_instance.bastion_instance.private_ip}/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  lifecycle {
+      create_before_destroy = true
+  }
+}
+
+resource "aws_key_pair" "gh_key" {
+  key_name   = var.tf_key_name
+  public_key = tls_private_key.ed_key.public_key_openssh
+}
+
+resource "tls_private_key" "ed_key" {
+  algorithm = "ED25519"
+}
+
+resource "local_file" "TF-key" {
+  content = tls_private_key.ed_key.private_key_openssh
+  filename = var.ssh_key_filename
+  file_permission = "0400"
+}
+
+resource "aws_instance" "bastion_instance" {
+  ami           = "ami-04b54ebf295fe01d7"
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.bas_sg.name]
+
+  associate_public_ip_address = true
+
+  key_name = var.tf_key_name
+
+  tags = {
+    Name = "BastionInstance"
+  }
+}
+
+resource "aws_instance" "secured_instance" {
+  ami           = "ami-04b54ebf295fe01d7"
+  instance_type = "t3.micro"
+  security_groups = [aws_security_group.sec_sg.name]
+
+  associate_public_ip_address = true
+
+  key_name = var.tf_key_name
+
+  tags = {
+    Name = "SecuredInstance"
+  }
+}
+
+resource "null_resource" "append_to_file" {
+  provisioner "local-exec" {
+    command = <<EOT
+      echo "bastion ansible_host=${aws_instance.bastion_instance.public_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=$(pwd)/${var.ssh_key_filename}" > ../ansible/inventory.ini
+      echo "secured ansible_host=${aws_instance.secured_instance.public_ip} private_ip=${aws_instance.secured_instance.private_ip} ansible_ssh_user=ec2-user ansible_ssh_private_key_file=$(pwd)/${var.ssh_key_filename}" >> ../ansible/inventory.ini
+    EOT
+  }
+}
